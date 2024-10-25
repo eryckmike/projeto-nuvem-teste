ab5396f37d3a7a9426053db5399b4d488b89e6ef
Eryck <eryckmike@gmail.com>
2024-10-24 21:29:18 -0300

    aletrado

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get("/", function(req, res) {
 
 // Rota 2: Sobre
 app.get("/banguela", function(req, res) {
-    res.send("<h1>Sobre Banguela</h1><p>Banguela é um gatinho preto muito fofo</p>");
+    res.send("<h1>Sobre Banguela</h1><p>Banguela é um gatinho preto muito fofo, e peludo</p>");
 });
 
 // Rota 3: Contato
